16ac7ab848bb0e2766fc8ebe900f175d6ebcc9ac
HetWorks <HetWorks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-16 14:32:54 +0000

    Adding first version
    git-svn-id: https://plugins.svn.wordpress.org/wp-image-shrinker/trunk@1091405 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ hetworkstinypng.php
@@ -0,0 +1,318 @@
+<?php
+/**
+ * Plugin Name: WordPress Image shrinker
+ * Plugin URI: http://www.hetworks.nl
+ * Description: Reduce image file sizes drastically and improve performance and Pagespeed score using the TinyPNG API within WordPress. Works for both PNG and JPG images.
+ * Version: 1.0
+ * Author: HETWORKS
+ * Author URI: http://www.hetworks.nl
+ * License: GPLv2 or later
+ */
+
+add_action('admin_menu', 'hetworkstinypng_createoptionspage');
+
+function hetworkstinypng_createoptionspage() {
+	add_menu_page('Image shrinker', 'Image shrinker', 'administrator', 'hetworkstinypng_optionspage', 'hetworkstinypng_displayoptionspage');
+	add_action( 'admin_init', 'hetworkstinypng_registersettings' );
+}
+
+function hetworkstinypng_registersettings() {
+	register_setting('hetworkstinypng_settings', 'tinypng_apikey');
+}
+
+/*
+* Link naar settings page 
+*/
+function hetworkstinypng_add_settings_link( $links ) {
+    $settings_link = '<a href="options-general.php?page=hetworkstinypng_optionspage">' . __( 'Settings' ) . '</a>';
+    array_push( $links, $settings_link );
+  	return $links;
+}
+$plugin = plugin_basename( __FILE__ );
+add_filter( "plugin_action_links_$plugin", 'hetworkstinypng_add_settings_link' );
+
+/*
+* Options pagina
+*/
+function hetworkstinypng_displayoptionspage() { ?>
+
+<div class="wrap">
+	<h2>WordPress Image shrinker by HETWORKS</h2>
+	<p>In order for this plugin to work you need to enter an API key from TinyPNG. You can generate your key on <a href="https://tinypng.com/developers" target="_blank">https://tinypng.com/developers</a>.
+	An API key is free for the first 500 images each month. If you have more images you
+	need to pay a small amount.</p>
+	<form method="post" action="options.php">
+	<?php settings_fields( 'hetworkstinypng_settings' ); ?>
+    <?php do_settings_sections( 'hetworkstinypng_settings' ); ?>
+	<table class="form-table">
+		<tr valign="top">
+			<th scope="row">Your TinyPNG API Key</th>
+			<td><input type="text" name="tinypng_apikey" style="min-width: 300px;" value="<?php echo esc_attr( get_option('tinypng_apikey') ); ?>" /></td>
+		</tr>
+	</table>
+	
+	<?php submit_button(); ?>
+</form>
+	<p>With the button below you can reprocess all the images in your Media library. You will see a great improvement in your Google Pagespeed score afterwards.</p>
+	<?php if (get_option('tinypng_apikey') != '') { $bclasses = 'secondary hetworkstinypng_ajax_button'; } else { $bclasses = 'secondary disabled'; } ?>
+	<?php submit_button('TinyPNG all current images', $bclasses); ?> <div class="bespaarddiv">Space saved: <span class="bespaard"></span> (<span class="ratio"></span>%)</div>
+	<table class="hetworkstinypng_images" style="display: none;">
+		<thead>
+			<tr>
+				<th>Image</th><th>Image name</th>
+					<th>full</th>
+					<?php foreach (get_intermediate_image_sizes() as $imagesize) { ?>
+						<th><?= $imagesize; ?></th>
+					<?php } ?>
+			</tr>
+		</thead>
+		<tbody>
+		
+		</tbody>
+	</table>
+	<p>Do you see the great advantage of this plugin? Please donate a small amount: <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+		<input type="hidden" name="cmd" value="_s-xclick">
+		<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHTwYJKoZIhvcNAQcEoIIHQDCCBzwCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYBz5DO3gYVjfHJgzZGCZIX82QZaUg26n3GK6BBOZy/NoUC3QncBp3LRgC+gZTKBD+Nx08/P72XHxVFUaXPylZmxJHyTpIiq7kyV7PLHiWvw8WAYYEi+tuoFvBWhIZeYZ4IHMKuTcwxaVEc99kjVdzkchEgiIuPEnCcBUrPSVWTkxzELMAkGBSsOAwIaBQAwgcwGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIEFR5BbhxRoeAgag98ToUdBQU/MF0eZqfSR+IjwKi9JtCmcalI7XCBP7CQufNYxKRhtfJJ+H6Vn5NxgC/6Z4Qap/j+Jcl/ynmnXDtZAGq7dQwsGYy+/bW4Pw1x+3GwR4mmScg/h+TgF7ZK3CihsjD7BZDDn8AGPSsdDISd3uI2AZQf1xlGIEOg6DL+XAfV0REYnPMLsccAHslGU3ObSj8ij9LJBfUgNLO41Vl+SwNDJb8bHqgggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xNTAyMTIxNTI5NTRaMCMGCSqGSIb3DQEJBDEWBBSrSPS7+pdRDZK0ZXLjMgIJNB9nijANBgkqhkiG9w0BAQEFAASBgJAHwMHuBy786eshVDhPSicwaT9mI5MQlQsxL+KXbjV4BKbVD27k6P12rjldsVx4AmfS2kWxjSG8psb13lnePPiJGFPdkOajD1lFVLwyS+uEcxCjjQ620XknY/ujAZFLtEehVFVOOfasa6cZ8iJyxdXydJVAFr01DWQRDiLDKuLF-----END PKCS7-----
+		">
+		<input type="image" src="https://www.paypalobjects.com/nl_NL/NL/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal, de veilige en complete manier van online betalen.">
+		<img alt="" border="0" src="https://www.paypalobjects.com/nl_NL/i/scr/pixel.gif" width="1" height="1">
+		</form>
+	</p>
+</div>
+<?php
+}
+
+add_action( 'admin_footer', 'hetworkstinypng_ajaxactionrequest' );
+
+function hetworkstinypng_ajaxactionrequest() { ?>
+	<script type="text/javascript" >
+	var voor = 0;
+	var na = 0;
+	var bespaard = 0;
+	jQuery(document).ready(function($) {
+		
+		$(".hetworkstinypng_ajax_button").click(function() {
+			if (!$(this).hasClass('disabled')) {
+				$(this).addClass('disabled');
+				$(".bespaarddiv").show().find('span.bespaard').html(bytesToSize(bespaard));
+				var data = {
+					'action': 'hetworkstinypng_ajaxaction_getimages'
+				};
+
+				$.post(ajaxurl, data, function(response) {
+					$("table.hetworkstinypng_images tbody").html('').parent('table').show();
+					$.each($.parseJSON(response), function(i,v) {
+						if (v.status != 'done') {
+							$("table.hetworkstinypng_images tbody").append('<tr data-id="'+v.id+'" data-basename="'+v.basename+'" data-url="'+v.url+'" data-done="false" data-sizes=\''+v.sizes+'\'><td><img class="hetworkstinypng_thumb" src="'+v.thumb+'" /></td><td>'+v.basename+'</td><td class="sizetd" data-tdsize="full"></td><?php foreach (get_intermediate_image_sizes() as $imagesize) { ?><td class="sizetd" data-tdsize="<?= $imagesize; ?>"></td><?php } ?></tr>');
+						} else {
+							$("table.hetworkstinypng_images tbody").append('<tr data-id="'+v.id+'" data-basename="'+v.basename+'" data-url="'+v.url+'" data-done="true" data-sizes=\''+v.sizes+'\'><td><img class="hetworkstinypng_thumb" src="'+v.thumb+'" /></td><td>'+v.basename+'</td><td class="sizetd" data-tdsize="full"></td><?php foreach (get_intermediate_image_sizes() as $imagesize) { ?><td class="sizetd" data-tdsize="<?= $imagesize; ?>"></td><?php } ?></tr>');
+						}
+					});
+					$(".sizetd").html('<span class="dashicons dashicons-clock"></span>');
+					$("tr[data-done='true'] .sizetd").html('<span class="dashicons dashicons-yes"></span>');
+					hetworkstinypng_ajax_sendnextimage();
+					
+				});
+			}
+		});
+	});
+	
+	function hetworkstinypng_ajax_sendnextimage() {
+		var nextimage = jQuery("table.hetworkstinypng_images tbody tr[data-done='false']").first();
+		if (nextimage.length != 0) {
+			nextimage.attr('data-done', 'true');
+			var id = nextimage.attr('data-id');
+			sizes = jQuery.parseJSON(nextimage.attr('data-sizes'));
+			jQuery.each(sizes, function(size,url) {
+				var tdata = {
+					'action': 'hetworkstinypng_ajaxaction_tinypngimage',
+					'url': url,
+					'id': id
+				};
+				jQuery.ajax({
+					type: 'POST',
+					url: ajaxurl,
+					data: tdata,
+					beforeSend: function() {
+						nextimage.find("td[data-tdsize='"+size+"']").html('').append('<img src="<?php echo plugins_url( 'img/ajax-loader.gif', __FILE__ ); ?>"  />');
+					}, 
+					success: completeHandler = function(response) {
+						res = jQuery.parseJSON(response);
+						if (typeof res.error === 'undefined') {
+							nextimage.find("td[data-tdsize='"+size+"']").html('').append('<table class="intable"><tr><td>From:</td><td>' + bytesToSize(res.input.size) + '</td></tr><tr><td colspan="2"><span class="dashicons dashicons-yes"></span></td></tr><tr><td>To:</td><td>' + bytesToSize(res.output.size) + '</td></tr></table>'); 
+							voor =  voor + res.input.size;
+							na = na + res.output.size;
+							jQuery("span.ratio").html((100 - ((na / voor) * 100)).toFixed(2));
+							bespaard = bespaard + (res.input.size - res.output.size);
+							jQuery("span.bespaard").html(bytesToSize(bespaard));
+							if (size == 'full') {
+								console.log(id);
+								hetworkstinypng_ajax_sendnextimage();
+								var donedata = {
+									'action': 'hetworkstinypng_ajaxaction_tinypngdone',
+									'id': id
+								};
+								jQuery.ajax({
+									type: 'POST',
+									url: ajaxurl,
+									data: donedata,
+									success: completeHandler = function(response) {
+										console.log(response);
+									}
+								});
+							}
+						} else {
+							nextimage.find("td[data-tdsize='"+size+"']").html('').append('<span class="dashicons dashicons-no-alt"></span><br />' + res.message);
+							if (res.error == 'TooManyRequests') {
+								jQuery("div.bespaarddiv").css('color', 'red').html(res.message);
+							}
+						}
+					}, 
+					error: errorHandler = function(data) {
+						console.log('error');
+					}
+				});
+			});
+		} else {
+			console.log('done');
+		}
+	}
+
+	function bytesToSize(bytes) {
+	   if(bytes == 0) return '0 Byte';
+	   var k = 1000;
+	   var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+	   var i = Math.floor(Math.log(bytes) / Math.log(k));
+	   return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
+	}
+	</script>
+	<style>
+	table.hetworkstinypng_images { border-spacing: 0; border: 1px solid grey; }
+	table.hetworkstinypng_images td, table.hetworkstinypng_images th {padding: 16px; }
+	table.hetworkstinypng_images td img.hetworkstinypng_thumb { height: 32px; }
+	table.intable { margin: 0 auto; }
+	table.intable td { padding: 2px; height: auto; }
+	table.hetworkstinypng_images td.sizetd { text-align: center; width: 130px; padding: 0; border-top: 1px solid grey; border-left: 1px solid grey; }
+	.bespaarddiv { display: none; margin-bottom: 20px;font-size: 16px;font-weight: bold; }
+	</style> <?php
+}
+
+add_action( 'wp_ajax_hetworkstinypng_ajaxaction_getimages', 'hetworkstinypng_ajaxaction_getimages' );
+add_action( 'wp_ajax_hetworkstinypng_ajaxaction_tinypngimage', 'hetworkstinypng_ajaxaction_tinypngimage' );
+add_action( 'wp_ajax_hetworkstinypng_ajaxaction_tinypngdone', 'hetworkstinypng_ajaxaction_tinypngdone' );
+
+function hetworkstinypng_ajaxaction_getimages() {
+	$response = '';	
+	$query_images_args = array(
+		'post_type' => 'attachment', 'post_mime_type' =>'image', 'post_status' => 'inherit', 'posts_per_page' => -1, 'meta_compare' => 'NOT EXISTS',
+	);
+
+	$query_images = new WP_Query( $query_images_args );
+	$images = Array();
+	$i = 0;
+	foreach ( $query_images->posts as $image) {
+		$images[$i]['url'] = wp_get_attachment_url( $image->ID );
+		$thumb = wp_get_attachment_image_src( $image->ID, 'thumbnail' );
+		$images[$i]['thumb'] = $thumb[0];
+		$images[$i]['basename'] = basename($images[$i]['url']);
+		$images[$i]['id'] = $image->ID;
+		$images[$i]['sizes'] = Array();
+		foreach (get_intermediate_image_sizes() as $imagesize) {
+			$size = wp_get_attachment_image_src( $image->ID, $imagesize);
+			$images[$i]['sizes'][$imagesize] = $size[0];
+		}
+		$images[$i]['sizes']['full'] = $images[$i]['url'];
+		$images[$i]['sizes'] = json_encode($images[$i]['sizes']);
+		$images[$i]['status'] = get_post_meta($image->ID, 'hetworkstinypng_status', true);
+		$i++;
+	}
+	
+	echo json_encode($images);
+	wp_die(); // Nodig voor propere ajax response
+}
+
+function hetworkstinypng_ajaxaction_tinypngimage() {
+	$key = get_option('tinypng_apikey');
+	$url = $_POST['url'];
+	$input = $url;
+	$output = '../../'. parse_url($url, PHP_URL_PATH);
+	$response = hetworkstinypng_tinypngrequest($input, $output, $key);
+	echo $response;
+	wp_die(); // Nodig voor propere ajax response
+}
+
+function hetworkstinypng_ajaxaction_tinypngdone() {
+	$id = $_POST['id'];
+	echo update_post_meta($id, 'hetworkstinypng_status', 'done');
+	wp_die(); // Nodig voor propere ajax response
+}
+
+/*
+* Na upload, voor wp_handle_upload, image door TinyPNG gooien
+*/
+add_filter('wp_handle_upload_prefilter', 'hetworkstinypng_uploadevent' );
+add_filter('wp_generate_attachment_metadata', 'hetworkstinypng_metadata', 10, 2 );
+
+function hetworkstinypng_uploadevent( $file ){
+    
+	if (get_option('tinypng_apikey') != '') {
+		if ($file['type'] == 'image/jpeg' OR $file['type'] == 'image/png') {
+			$key = get_option('tinypng_apikey');
+			$input = $file['tmp_name'];		
+			$output = $file['tmp_name'];
+			hetworkstinypng_tinypngrequest($input, $output, $key);
+		}
+	}
+    return $file;
+}
+
+function hetworkstinypng_metadata($metadata, $attachment_id) {
+	if (get_option('tinypng_apikey') != '') {
+		update_post_meta($attachment_id, 'hetworkstinypng_status', 'done');
+	}
+	return $metadata;
+}
+
+/*
+* de request naar TinyPNG toe
+*/
+function hetworkstinypng_tinypngrequest($input, $output, $key) {
+		$request = curl_init();
+		curl_setopt_array($request, array(
+		  CURLOPT_URL => "https://api.tinypng.com/shrink",
+		  CURLOPT_USERPWD => "api:" . $key,
+		  CURLOPT_POSTFIELDS => file_get_contents($input),
+		  CURLOPT_BINARYTRANSFER => true,
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_HEADER => true,
+		  CURLOPT_CAINFO => dirname(__FILE__) . '/cacert.pem',
+		  CURLOPT_SSL_VERIFYPEER => true 
+		));
+
+		$response = curl_exec($request);
+		$json = explode("\r\n\r\n", $response);
+		$json = $json[2];
+		$jsonarr = json_decode($json);
+		if (curl_getinfo($request, CURLINFO_HTTP_CODE) === 201) {
+
+		  $headers = substr($response, 0, curl_getinfo($request, CURLINFO_HEADER_SIZE));
+		  foreach (explode("\r\n", $headers) as $header) {
+			if (substr($header, 0, 10) === "Location: ") {
+			  $request = curl_init();
+			  curl_setopt_array($request, array(
+				CURLOPT_URL => substr($header, 10),
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_CAINFO => dirname(__FILE__) . '/cacert.pem',
+				CURLOPT_SSL_VERIFYPEER => true
+			  ));
+			  file_put_contents($output, curl_exec($request));
+			  return $json;
+			}
+		  }
+		} else {
+			if ($jsonarr->error == 'TooManyRequests') {
+				return $json;
+			}
+			return 'fail: error: {' . curl_error($request) . $json . '}';
+		}
+}
---
+++ readme.txt
@@ -0,0 +1,70 @@
+=== WordPress Image shrinker ===
+Contributors: HetWorks
+Plugin Name: HETWORKS TinyPNG
+Plugin URI: http://www.hetworks.nl
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=6JSXUB6Q9SD2U&lc=NL&item_name=HETWORKS&item_number=TinyPNG%20for%20WordPress&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
+Version: 0.1.1
+Tested up to: 4.1
+Author: Anne Rozema
+Author URI: http://www.hetworks.nl
+Tags: images
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Reduce image file sizes drastically and improve performance and Pagespeed score using the TinyPNG API within WordPress. Works for both PNG and JPG images.
+
+== Description ==
+
+You probably know that the speed of your website is important for both user experience and for your search engine ranking. The people 
+at TinyPNG.com offered this great service where you could upload your PNG images and shortly also your JPG images, to have the file size 
+shrinked to much lower amount of data, without a noticable reduce of image quality. And that is great! Compare your Google pagespeed score 
+before and after you shrinked your images. You will gain many points.
+
+The disadvantage of using TinyPNG is that you had to use the service upfront, before you could upload your images into your WordPress 
+medialibrary you had to upload them to TinyPNG and download the shrinked files again. When the images were already in your medialibrary you 
+had to do this all over, also for the images that were cropped by WordPress into the different filesizes. With this plugin that is all history. 
+You just upload your images and as part of the proces these images are processed by TinyPNG.   
+
+This is what TinyPNG say themselves:
+How does it work? TinyPNG uses smart lossy compression techniques to reduce the file size of your PNG files. By selectively decreasing the 
+number of colors in the image, fewer bytes are required to store the data. The effect is nearly invisible but it makes a very large 
+difference in file size!
+
+
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Upload the HETWORKS TinyPNG folder to the plugins directory in your WordPress installation
+2. Activate the plugin
+3. Navigate to the HETWORKS TinyPNG UI Menu
+4. Generate your TinyPNG API key from https://tinypng.com/developers and enter it on the Settings page.
+
+That's it! From now on all your new images are shrinked on the fly!
+
+
+== Frequently Asked Questions ==
+
+= The plugin doesn't work =
+
+Did you insert your TinyPNG API key? You can retreive it from https://tinypng.com/developers It is free for up to 500 images per month.
+
+= What about the images I did upload before installing your plugin? =
+
+On the settings page you find a button to reprocess all images in the library again.
+
+= Why is uploading images taking more time =
+
+After the image is uploaded WordPress shows you it is finished (100%), but right then your image is processed by TinyPNG. That may take a few extra seconds.
+
+
+== Screenshots ==
+
+
+
+== Changelog ==
+
+= 1.0 =
+* First release
+
